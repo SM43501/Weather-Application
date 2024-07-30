5ac1e878ee54e4937b39ec90a31753340392b856
majum <majum@Snehasish>
2024-07-30 18:48:47 +0530

    initial commit

---
+++ Weather Application/src/main/java/MyPackage/MyServlet.java
@@ -0,0 +1,85 @@
+package MyPackage;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.Scanner;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public MyServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.sendRedirect("index.html");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String apiKey = "94c5de1b41c70647de1bfc0061dd0447";
+		String city = request.getParameter("city");
+		String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+		try {
+			URI uri = new URI(apiUrl);
+			URL url = uri.toURL();
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestMethod("GET");
+
+			InputStream inputStream = connection.getInputStream();
+			InputStreamReader reader = new InputStreamReader(inputStream);
+			Scanner scanner = new Scanner(reader);
+			StringBuilder responseContent = new StringBuilder();
+
+			while (scanner.hasNext()) {
+				responseContent.append(scanner.nextLine());
+			}
+
+			scanner.close();
+
+			Gson gson = new Gson();
+			JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
+
+			long dateTimestamp = jsonObject.get("dt").getAsLong() * 1000;
+			String date = new Date(dateTimestamp).toString();
+
+			double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+			int temperatureCelsius = (int) (temperatureKelvin - 273.15);
+
+			int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+
+			double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+
+			String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+
+			request.setAttribute("date", date);
+			request.setAttribute("city", city);
+			request.setAttribute("temperature", temperatureCelsius);
+			request.setAttribute("weatherCondition", weatherCondition);
+			request.setAttribute("humidity", humidity);
+			request.setAttribute("windSpeed", windSpeed);
+			request.setAttribute("weatherData", responseContent.toString());
+
+			connection.disconnect();
+
+			request.getRequestDispatcher("index.jsp").forward(request, response);
+		} catch (IOException | URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+}
